b71bfbd0c3952d08e8e38e69a75af0389dd9459e
Shiwen An <sweynan@icloud.com>
2022-07-13 12:53:48 +0900

    Some update on emulation part, needs more progress on the plot

--- crosscheck_kl1/emulation.py
+++ crosscheck_kl1/emulation.py
@@ -108,47 +108,94 @@ def tree_online_PtRNNdR(tree, emu):
                 if (  dR> 0.3 and dR <3.0) : passPtRNNdR = true
     return [passPt,passPtdR,passRNN, passPtRNN, passPtRNNdR] 
 
-# def fill_hist_pt(tree):
-#     hist_m_offhltrnn = ROOT.TH1D("m_offhlt_rnn", "", 50, 0, 1)
-#     hist_m_offhltprong = ROOT.TH1D("m_offhlt_prong", "", 10, 0, 10)
-#     hist_m_offhltpt_r = ROOT.TH1D(
-#         "m_offhltpt_r", "", r_pt[0], r_pt[1], r_pt[2])
-#     hist_m_offhltpt_lead_r = ROOT.TH1D(
-#         "m_offhltpt_lead_r", "", r_pt[0], r_pt[1], r_pt[2])
-#     hist_m_offhltpt_sublead_r = ROOT.TH1D(
-#         "m_offhltpt_sublead_r", "", r_pt[0], r_pt[1], r_pt[2])
-#     hist_m_offhltptdeltaR = ROOT.TH1D("m_offhltptdeltaR", "", 50, -1, 4)
+def fill_hist_pt(tree, emu):
+    entries = range(tree.GetEntries())
+    hltoff = []
 
-#     hist_HLT_offhltrnn = ROOT.TH1D("HLT_offhlt_rnn", "", 50, 0, 1)
-#     hist_HLT_offhltprong = ROOT.TH1D("HLT_offhlt_prong", "", 10, 0, 10)
-#     hist_HLT_offhltpt_r = ROOT.TH1D(
-#         "HLT_offhltpt_r", "", r_pt[0], r_pt[1], r_pt[2])
-#     hist_HLT_offhltpt_lead_r = ROOT.TH1D(
-#         "HLT_offhltpt_lead_r", "", r_pt[0], r_pt[1], r_pt[2])
-#     hist_HLT_offhltpt_sublead_r = ROOT.TH1D(
-#         "HLT_offhltpt_sublead_r", "", r_pt[0], r_pt[1], r_pt[2])
-#     hist_HLT_offhltptdeltaR = ROOT.TH1D("HLT_offhltptdeltaR", "", 50, -1, 4)
+    hist_m_offhltrnn = ROOT.TH1D("m_offhlt_rnn", "", 50, 0, 1)
+    hist_m_offhltprong = ROOT.TH1D("m_offhlt_prong", "", 10, 0, 10)
+    hist_m_offhltpt_r = ROOT.TH1D(
+        "m_offhltpt_r", "", r_pt[0], r_pt[1], r_pt[2])
+    hist_m_offhltpt_lead_r = ROOT.TH1D(
+        "m_offhltpt_lead_r", "", r_pt[0], r_pt[1], r_pt[2])
+    hist_m_offhltpt_sublead_r = ROOT.TH1D(
+        "m_offhltpt_sublead_r", "", r_pt[0], r_pt[1], r_pt[2])
+    hist_m_offhltptdeltaR = ROOT.TH1D("m_offhltptdeltaR", "", 50, -1, 4)
+
+    hist_HLT_offhltrnn = ROOT.TH1D("HLT_offhlt_rnn", "", 50, 0, 1)
+    hist_HLT_offhltprong = ROOT.TH1D("HLT_offhlt_prong", "", 10, 0, 10)
+    hist_HLT_offhltpt_r = ROOT.TH1D(
+        "HLT_offhltpt_r", "", r_pt[0], r_pt[1], r_pt[2])
+    hist_HLT_offhltpt_lead_r = ROOT.TH1D(
+        "HLT_offhltpt_lead_r", "", r_pt[0], r_pt[1], r_pt[2])
+    hist_HLT_offhltpt_sublead_r = ROOT.TH1D(
+        "HLT_offhltpt_sublead_r", "", r_pt[0], r_pt[1], r_pt[2])
+    hist_HLT_offhltptdeltaR = ROOT.TH1D("HLT_offhltptdeltaR", "", 50, -1, 4)
+
+    for entry in entries:
+        tree.GetEntry(entry)
+        L1_1 = getattr(tree, "L1_J25")
+        L1_2 = getattr(tree, "L1_ETA25")
+        # Bit confused about this point of trigger use
+        if taus[t] == "r22_Pass":
+            HLT_1 = getattr(tree, "HLT_J25_r22")
+            HLT = "HLT_J25_r22"
+        elif taus[t] == "r22_PassFail":
+            HLT_1 = getattr(tree, "HLT_J25_r22")
+            HLT = "HLT_J25_r22"
+        elif taus[t] == "Tau0_Pass":
+            HLT_1 = getattr(tree, "HLT_J25_Tau0")
+            HLT = "HLT_J25_Tau0"
+        elif taus[t] == "Tau0_PassFail":
+            HLT_1 = getattr(tree, "HLT_J25_Tau0")
+            HLT = "HLT_J25_Tau0"
+
+        # Selection
+        select = True
+        select = select and (len(tree.Offline_Matched_Taus) >= 2)
+        if(select):
+            select = select and (tree.Offline_Matched_Taus[0].Pt() > 20)
+            select = select and (tree.Offline_Matched_Taus[1].Pt() > 12)
+        if(len(tree.Off_Matched_TauIDl)<2): continue
+        for i in range(len(tree.Off_Matched_TauIDl)):
+                # print(tree.Off_Matched_TauIDl[i])
+                if(tree.Off_Matched_TauIDl[0] == True
+                 and tree.Off_Matched_TauIDl[1] == True):
+                    select = select 
+                else:
+                    select = False
+        select = select and (L1_1 or L1_2)
+
+        # How to pass RNN loose ID?
+        if(select):
+            EMU_1 = tree_online_PtRNNdR(tree, emu)
+            if L1_1 and EMU_1[emu]:
+                for i in range(len(tree.TrigMatched_Taus_HLTptfl)) :
+                    if ( tree.TrigMatched_Taus_HLTptfl[i].Pt() < 35): continue
+                    for  j in range(len(tree.TrigMatched_Taus_HLTptfl)) :
+                        if (i==j): continue
+                        if ( tree.TrigMatched_Taus_HLTptfl[j].Pt() < 25): continue
+                        vec0 = tree.TrigMatched_Taus_HLTptfl[j].Vect()
+                        vec1 = tree.TrigMatched_Taus_HLTptfl[i].Vect()
+                        passPt = true
+                        pt0 = tree.TrigMatched_Taus_HLTptfl[j].Pt()
+                        pt1 = tree.TrigMatched_Taus_HLTptfl[i].Pt()
+                        rnn_m_0 = tree.TrigMatched_TauIDm_HLTptfl[j]
+                        rnn_m_1 = tree.TrigMatched_TauIDm_HLTptfl[i]
+                        rnn_l_0 = tree.TrigMatched_TauIDl_HLTptfl[j]
+                        rnn_l_1 = tree.TrigMatched_TauIDl_HLTptfl[i]
+                        rnn = rnn_region(pt0,pt1,rnn_m_0, rnn_m_1, rnn_l_0, rnn_l_1)
+                        if (rnn): passPtRNN = true
+                        else: continue
+                        dR = vec0.DeltaR(vec1)
+                        if (  dR> 0.3 and dR <3.0) : 
+                            passPtRNNdR = true
+                            hist_m_offhltpt_r.Fill(tree.TrigMatched_Taus_HLTptfl[j].Pt())
+                            hist_m_offhltpt_r.Fill(tree.TrigMatched_Taus_HLTptfl[i].Pt())
+        
+    hist_print_compare([ hist_m_offhltpt_r ], ["Pt"], emu_order[0], 0)
 
 
-#         for i in range(len(tree.TrigMatched_Taus_HLTptfl)) :
-#             if ( tree.TrigMatched_Taus_HLTptfl[i].Pt() < 35): continue
-#             for  j in range(len(tree.TrigMatched_Taus_HLTptfl)) :
-#                 if (i==j): continue
-#                 if ( tree.TrigMatched_Taus_HLTptfl[j].Pt() < 25): continue
-#                 vec0 = tree.TrigMatched_Taus_HLTptfl[j].Vect()
-#                 vec1 = tree.TrigMatched_Taus_HLTptfl[i].Vect()
-#                 passPt = true
-#                 pt0 = tree.TrigMatched_Taus_HLTptfl[j].Pt()
-#                 pt1 = tree.TrigMatched_Taus_HLTptfl[i].Pt()
-#                 rnn_m_0 = tree.TrigMatched_TauIDm_HLTptfl[j]
-#                 rnn_m_1 = tree.TrigMatched_TauIDm_HLTptfl[i]
-#                 rnn_l_0 = tree.TrigMatched_TauIDl_HLTptfl[j]
-#                 rnn_l_1 = tree.TrigMatched_TauIDl_HLTptfl[i]
-#                 rnn = rnn_region(pt0,pt1,rnn_m_0, rnn_m_1, rnn_l_0, rnn_l_1)
-#                 if (rnn): passPtRNN = true
-#                 else: continue
-#                 dR = vec0.DeltaR(vec1)
-#                 if (  dR> 0.3 and dR <3.0) : passPtRNNdR = true
 
 
 
@@ -237,8 +284,8 @@ def emulation(input_root, t, emu):
                 if EMU_1[emu]:
                     numerator_emu = numerator_emu+1
                     for i in range(len(tree.TrigMatched_Taus_HLTptfl)):
-                        hist_m_offhltpt_r.Fill(
-                            tree.TrigMatched_Taus_HLTptfl[i].Pt(), 1)
+                        pt0 = tree.TrigMatched_Taus_HLTptfl[i].Pt()
+                        if(pt0 >25):hist_m_offhltpt_r.Fill(pt0, 1)
                         if(i == 0):
                             hist_m_offhltpt_lead_r.Fill(
                                 tree.TrigMatched_Taus_HLTptfl[0].Pt(), 1)
@@ -253,10 +300,20 @@ def emulation(input_root, t, emu):
                     for k in range(len(tree.TrigMatched_prong_HLTptfl)):
                         hist_m_offhltprong.Fill(
                             tree.TrigMatched_prong_HLTptfl[k], 1)  
-            if L1_1:
-                if HLT_1:
+            if L1_1 and HLT_1 and tree_online_PtRNNdR(tree,0):
                     numerator_hlt = numerator_hlt+1
-
+                    for i in range(len(tree.TrigMatched_Taus_HLTptfl)):
+                        pt0 = tree.TrigMatched_Taus_HLTptfl[i].Pt()
+                        if (pt0 >25): hist_HLT_offhltpt_r.Fill(pt0, 1)
+                        if(i == 0):
+                            hist_HLT_offhltpt_lead_r.Fill(
+                                tree.TrigMatched_Taus_HLTptfl[0].Pt(), 1)
+                        if(i == 1):
+                            hist_HLT_offhltpt_sublead_r.Fill(
+                                tree.TrigMatched_Taus_HLTptfl[1].Pt(), 1)
+                            vec0 = tree.TrigMatched_Taus_HLTptfl[0].Vect()
+                            vec1 = tree.TrigMatched_Taus_HLTptfl[1].Vect()
+                            hist_HLT_offhltptdeltaR.Fill(vec1.DeltaR(vec0))
                     for j in range(len(tree.TrigMatched_rnn_HLTptfl)):
                         hist_HLT_offhltrnn.Fill(tree.TrigMatched_rnn_HLTptfl[j], 1)
                     for k in range(len(tree.TrigMatched_prong_HLTptfl)):
@@ -275,22 +332,22 @@ def emulation(input_root, t, emu):
     # hltoff.append(hist_offhltpt_lead_r)
     # hltoff.append(hist_offhltpt_sublead_r)
 
-    # hltoff.append(hist_m_offhltrnn)
-    # hltoff.append(hist_m_offhltprong)
-    # hltoff.append(hist_m_offhltptdeltaR)
-    # hltoff.append(hist_m_offhltpt_lead_r)
-    # hltoff.append(hist_m_offhltpt_sublead_r)
+    hltoff.append(hist_m_offhltrnn)
+    hltoff.append(hist_m_offhltprong)
+    hltoff.append(hist_m_offhltptdeltaR)
+    hltoff.append(hist_m_offhltpt_r)
+    hltoff.append(hist_m_offhltpt_sublead_r)
 
-    # hltoff.append(hist_HLT_offhltrnn)
-    # hltoff.append(hist_HLT_offhltprong)
-    # hltoff.append(hist_HLT_offhltptdeltaR)
-    # hltoff.append(hist_HLT_offhltpt_lead_r)
-    # hltoff.append(hist_HLT_offhltpt_sublead_r)
+    hltoff.append(hist_HLT_offhltrnn)
+    hltoff.append(hist_HLT_offhltprong)
+    hltoff.append(hist_HLT_offhltptdeltaR)
+    hltoff.append(hist_HLT_offhltpt_r)
+    hltoff.append(hist_HLT_offhltpt_sublead_r)
 
-    # for i in range(5):
-    #     hist_print_compare([hltoff[i],hltoff[i+5], hltoff[i+10]],
-    #         ["off", "emu", "hlt"],
-    #         list_order[i], t)
+    for i in range(5):
+        hist_print_compare([hltoff[i],hltoff[i+5]],
+            ["emu", "hlt"],
+            list_order[i], t)
 
 
 def HLT(input_root, t):
